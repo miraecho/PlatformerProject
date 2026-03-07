2df40a648ba1cb1c865ecb9ff31b8ea6579750fc
miraecho <49175337+miraecho@users.noreply.github.com>
2026-03-06 21:55:52 -0800

    Pause Menu and Main Menu Screen

---
+++ Assets/Scripts/Pause Menu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject container;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            container.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ResumeButton() 
+    {
+        container.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuButton() 
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("");
+    }
+}
