69b7d8e7478806d3cd29eae01fba0f3b831111be
miraecho <49175337+miraecho@users.noreply.github.com>
2026-03-10 21:53:22 -0700

    Coin UI

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
     public AudioClip coinClip;
+    private TextMeshProUGUI coinText;
+
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("Coin Text").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") 
@@ -12,6 +19,7 @@ public class Coin : MonoBehaviour
             Player player = collision.gameObject.GetComponent<Player>();
             player.coins += 1;
             player.PlaySFX(coinClip, 0.4f);
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
